3971f3733cbf087203b6758f77a0553ad99c569d
amjadjibon <amjadjibon@users.noreply.github.com>
2026-05-31 15:07:09 +0000

    deploy: 66db39be2d310570f3156528d2a416a853db76d3

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-b50e97f3.js',
+                path_to_root + 'searchindex-0d4e5ada.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
